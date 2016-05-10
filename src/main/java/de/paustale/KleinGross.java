25413d3958d0ff6ab8544d6b060bf95e2d3f6ed1
Alexander Paust <alexander.paust@hs-furtwangen.de>
2016-05-10 16:10:15 +0200

    Kommentare

--- src/main/java/de/paustale/KleinGross.java
+++ src/main/java/de/paustale/KleinGross.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class KleinGross {
 	public static void main(String args[])
 	{
+		//Scannen Eingabe
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Eingabe: ");
+		//Eingabe in String
 		String eingabe = sc.next();
+		//Ausgabe
 		System.out.println("Das wurde eingegeben: " + eingabe);
+		//Eingabe in Groﬂbuchstaben umwandeln
 		String gross = eingabe.toUpperCase();
+		//Ausgabe
 		System.out.println("Umgewandelt: " + gross);
 	}
 }
